1d096136f17f3d43dfca30e2ffce2b937e88543c
setiyonoag <saiya.adzim@gmail.com>
2021-09-05 14:50:19 +0700

    Initial commit

---
+++ src/main/java/com/masyon/village/VillageSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.masyon.village;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VillageSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VillageSpringbootApplication.class, args);
+	}
+
+}
