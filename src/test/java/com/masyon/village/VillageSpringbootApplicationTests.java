9398124023f77295795e55f361d9930f96fc4309
setiyonoag <saiya.adzim@gmail.com>
2021-09-05 14:51:56 +0700

    Initial commit

---
+++ src/test/java/com/masyon/village/VillageSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.masyon.village;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VillageSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
